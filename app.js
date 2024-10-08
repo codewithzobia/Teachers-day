450716cbd69f900c3b5820d832ed4b3ad2768017
Zobia <zobiajawed5@gmail.com>
2024-10-08 21:56:01 +0500

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-alert("2 + 2 =");
-alert("4 Ans");
-alert("4 + 4 =");
-alert("8 Ans");
+window.alert("Happy Teacher's Day sir Rizwan Bhatti and sir Asad 👨‍💻👩🏻‍🎓💫");
+window.alert("Happy Teachers' Day! Your motivation drives us to succeed")
+window.alert("You’ve sparked our curiosity and fuelled our passion. Happy Teacher's Day! 👨‍💻👩🏻‍🎓💫");
+window.alert("To the teacher who pushes us to be our best, Happy Teachers' Day!")
+
+console.log("HAPPY TEACHERS DAY 💕👍🏻");
+console.log("YOU ARE THE BEST TEACHER EVER 👌🏻🫰🏻");
+console.log("GOD BLESS YOU WITH MAN HAPPINESS 🤲🏻");
+console.log("Thank you for shaping young minds and inspiring future generations");
+console.log("Happy Teachers Day to the person who has taught me more than just subjects");
+console.log("Thank you for believing in me and helping me reach my full potential.");
